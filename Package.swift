b4e6eb2bbe837135dd47ab33c1a9a7435d8e7f2a
Aurimas <bavarskiss@gmail.com>
2023-10-16 16:10:47 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,25 +4,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "MaliciousActor",
+    name: "MaliciousPackage",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MaliciousActor",
-            targets: ["MaliciousActor", "RemoteBinaryMaliciousActor"]),
+            name: "MaliciousPackage",
+            targets: [ "MaliciousBinaryLocal"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        .target(
-            name: "MaliciousActor",
-            dependencies: []),
-        .binaryTarget(
-            name: "RemoteBinaryMaliciousActor",
-            url: "https://github.com/bavarskis/maliciousActor/blob/main/MaliciousActor.xcframework.zip",
-            checksum: "432e4e157396fd9ed0ac5234392c4d9dfe9bb490cc6bcc91e030a22fbecef6b3"
-        )
+        .binaryTarget(name: "MaliciousBinary",
+                      url: "https://github.com/bavarskis/maliciousActor/blob/main/MaliciousActor.xcframework.zip",
+                      checksum: "432e4e157396fd9ed0ac5234392c4d9dfe9bb490cc6bcc91e030a22fbecef6b3")
     ]
 )
