bce4f270237a2439ed7a855fa6b15afdda33a9e9
Aurimas <bavarskiss@gmail.com>
2023-10-16 11:22:14 +0200

    Add framework and package manifest

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MaliciousActor",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "MaliciousActor",
+            targets: ["MaliciousActor", "RemoteBinaryMaliciousActor"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "MaliciousActor",
+            dependencies: []),
+        .binaryTarget(
+            name: "RemoteBinaryMaliciousActor",
+            url: "https://url/to/some/remote/xcframework.zip",
+            checksum: "432e4e157396fd9ed0ac5234392c4d9dfe9bb490cc6bcc91e030a22fbecef6b3"
+        )
+    ]
+)
